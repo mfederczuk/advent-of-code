9d2150f1cb0bf5dbf1afa595ef667b3b372ca779
Michael Federczuk <federczuk.michael@protonmail.com>
2021-12-07 13:59:12 +0100

    Advent of Code 2021 - Day 6 - Part 2

---
+++ 2021/day06/part2.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 Michael Federczuk
+ *
+ * SPDX-License-Identifier: MPL-2.0 AND Apache-2.0
+ */
+
+#include "day06_utils.hpp"
+
+int main() {
+	print_lantern_fish("input.txt", 256);
+	return 0;
+}
