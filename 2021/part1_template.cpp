aacf60ff4a14e483817e68af2d67b16ebd986c79
Michael Federczuk <federczuk.michael@protonmail.com>
2021-12-12 15:54:06 +0100

    Update `2021/init-day`

---
+++ 2021/part1_template.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 Michael Federczuk
+ *
+ * SPDX-License-Identifier: MPL-2.0 AND Apache-2.0
+ */
+
+#include "../aoc2021.hpp"
+#include <istream>
+
+ANSWER solution(std::istream& input) {
+	return INVALID; // TODO
+}
